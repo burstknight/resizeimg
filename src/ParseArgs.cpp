b6a39e399993a4d19fd488726e007e17f49876ee
burstknight <burstknight@gmail.com>
2026-09-09 15:51:30 +0800

    feat: Impl `ParseArgs::parseArgs()`
    - Target:
      This repo need be add a source file to implement the function
      `ParseArgs::parseArgs()`.
    
    - Changed
      Add the source file `ParseArgs.cpp` to implement the function
      `ParseArgs::parseArgs()`.

---
+++ src/ParseArgs.cpp
@@ -0,0 +1,51 @@
+#include "ParseArgs.h"
+#include "CLI/CLI.hpp"
+#include <cstdint>
+#include <cstdio>
+#include <memory>
+#include <string>
+
+using namespace ParseArgs;
+using namespace std;
+using namespace CLI;
+
+unique_ptr<Args_s> ParseArgs::parseArgs(int argc, char** argv) {
+	App oApp("resizeimg", "A command line tool can resize image.");
+	unique_ptr<Args_s> poArgs = make_unique<Args_s>();
+
+	oApp.add_option("input_path", poArgs->m_sInputImagePath,
+			"Give an input image to resize.")
+		->option_text(" ")
+		->required()
+		->check(CLI::ExistingFile);
+	oApp.add_option("result_path", poArgs->m_sResultImagePath,
+			"Give the path to store the result image. The default value is `result.png`")
+		->option_text(" ")
+		->default_str("result.png");
+	oApp.add_option("-W,--width", poArgs->m_iWidth,
+			"Give the expected width to resize the input image. The default value is 800")
+		->default_val(800)
+		->check(CLI::Range(1, INT32_MAX));
+	oApp.add_option("-H,--height", poArgs->m_iHeight,
+			"Give the expected height to resize the input image. The default value is 800.")
+		->default_val(800)
+		->check(CLI::Range(1, INT32_MAX));
+
+	oApp.usage("Usage: " + string(argv[0]) + " [options] <input_path> [result_path]");
+	argv = oApp.ensure_utf8(argv);
+
+	try {
+		oApp.parse(argc, argv);
+	} catch (const CLI::CallForHelp& e) {
+		oApp.exit(e);
+
+		return nullptr;
+	} catch (const CLI::Error& e) {
+		fprintf(stderr, "Error: Failed to parse command line arguments.\n  Details:\t%s\n\n", e.what());
+		oApp.exit(e);
+
+		return nullptr;
+	} // End of try-catch
+
+	return poArgs;
+} // End of parseArgs
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include "ParseArgs.h"
 
-int main (int argc, char** argv) {
-	printf("Hello world!\n");
+using namespace std;
+
+int main(int argc, char** argv) {
+	auto poArgs = ParseArgs::parseArgs(argc, argv);
+	if (nullptr == poArgs) {
+		exit(1);
+	} // End of if-condition
+
+	printf("Input image: %s\n", poArgs->m_sInputImagePath.c_str());
+	printf("Result image: %s\n", poArgs->m_sResultImagePath.c_str());
+	printf("Width: %d\n", poArgs->m_iWidth);
+	printf("Height: %d\n", poArgs->m_iHeight);
 	return 0;
 } // End of main
